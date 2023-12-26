edbf39ec75a8fdf801c373c2d26e8ce10b34dada
Pei <153276259+Peuuuu@users.noreply.github.com>
2023-12-26 16:12:58 +0800

    Calculator.java
    looping calculator using switch case and while

---
+++ CC2/midterms/Calculator.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class Calculator {
+Comment
+
+
+ 
+Leave a comment
+ 
+    public static void main(String[] args) {
+  Scanner pey = new Scanner (System.in);
+
+  int inputOne , inputTwo; 
+  int choice;
+  int answer = 0;
+  boolean condition = true;
+
+
+     System.out.println("_______________________\n Calculator\n_______________________" );     
+     System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exit");
+
+     while (condition){
+    //scanners
+
+        System.out.print("First Input:");
+        inputOne = pey.nextInt();
+        System.out.print("Second Input:");
+        inputTwo = pey.nextInt();
+        System.out.print("Choose your fighter:"); 
+        choice = pey.nextInt();
+
+     switch (choice){
+         case 1: 
+             answer = (inputOne) + (inputTwo);
+             System.out.println(("Sum:") + (answer));
+             break;
+         case 2: 
+             answer = (inputOne) - (inputTwo);
+             System.out.println(("Difference:") + (answer));
+             break;
+         case 3: 
+             answer = (inputOne) * (inputTwo);
+             System.out.println(("Product:") + (answer));
+             break;
+         case 4: 
+             answer = (inputOne) / (inputTwo);
+             System.out.println(("Quotient:") + (answer));
+             break;
+         case 5: 
+             System.out.println("Goodbye, Thank you!");
+             condition = false;
+             break;
+         default:
+             System.out.println("Invalid Choice!");
+
+     }      
+
+
+
+     }
+
+    }
+
+}
