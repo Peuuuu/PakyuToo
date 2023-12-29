d8e9fab4bf4b76dc02778b6d7c2644bd5aceef1a
Pei <153276259+Peuuuu@users.noreply.github.com>
2023-12-29 15:03:51 +0800

    Scenario One
    string concatenation

---
+++ CC2/finals/ScenarioOne.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ScenarioOne {
+    public static void main(String[] args) {
+        Scanner pey = new Scanner (System.in);
+
+            String one = pey.nextLine();
+            System.out.print("Enter second string:");
+            String two = pey.nextLine();
+            String Three = one + two;
+
+            System.out.println(Three);
+
+      //basta yon    
+        }
+
+    }
+
+
+    
+
