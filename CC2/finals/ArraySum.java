1df899e47ec6a82111b3267dda1a92a3071591da
Pei <153276259+Peuuuu@users.noreply.github.com>
2024-01-01 21:15:31 +0800

    Sum of the Array

---
+++ CC2/finals/ArraySum.java
@@ -0,0 +1,16 @@
+public class ArraySum {   
+    public static void main(String[] args) {
+        
+        
+        int input [] = {10,12,13,14,18,17};
+        int sum = 0;
+            for (int i = 1; i<input.length;i++ ){
+                sum += input [i];
+                  
+            }
+            System.out.println((" Total sum:")+ sum); 
+    }
+}
+
+    
+
