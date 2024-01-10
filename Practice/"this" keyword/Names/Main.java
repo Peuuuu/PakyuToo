50b30e499bdf6ab38c87dbff4158af25944de818
Pei <153276259+Peuuuu@users.noreply.github.com>
2024-01-10 20:40:34 +0800

    Main
    sample for global and local variables and using "this" keyword in inheritance

---
+++ Practice/"this" keyword/Names/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Person Pey = new Person ("Samson","Jazzmine Faye", "Hernandez", 19, 9);
+        System.out.println(Pey.LastName);
+    }
+    
+}
