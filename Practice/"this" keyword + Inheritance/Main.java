c1eb4a441459cc7304ab073a1a0677294314ff37
Pei <153276259+Peuuuu@users.noreply.github.com>
2024-01-11 14:44:57 +0800

    Main

---
+++ Practice/"this" keyword + Inheritance/Main.java
@@ -0,0 +1,9 @@
+public class Students {
+
+    public static void main(String[] args) {
+        
+        Information enrolleeOne = new Information ("Paolo","Gerona Tarlac",18,2005);
+        enrolleeOne.print();
+        
+    }
+}
