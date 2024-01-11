d747a4a9115b07d8650bb1ce5f56ff919674a7bb
Pei <153276259+Peuuuu@users.noreply.github.com>
2024-01-11 14:15:13 +0800

    Class

---
+++ Practice/Basic Calculator/Class.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+        Scanner pey = new Scanner (System.in);
+        Factors total = new Factors();
+         
+        System.out.print("Enter num One:");
+        int a = pey.nextInt();
+        System.out.print("Enter num Two:");
+        int b = pey.nextInt();
+        
+        total.add(a,b);
+        total.subtract(a, b);
+        total.multiply(a, b);
+        total.divide(a, b);
+        total.square(a);
+        
+    }
+}
