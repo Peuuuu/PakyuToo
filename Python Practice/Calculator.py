f8a3d5aef578eb7197f9add7e0c607a7873b401d
Pei <153276259+Peuuuu@users.noreply.github.com>
2024-01-24 10:18:16 +0800

    Calculator

---
+++ Python Practice/Calculator.py
@@ -0,0 +1,27 @@
+#comment ey
+numOne = int(input("Enter first input: "))
+numTwo = int(input ("Enter second input: "))
+
+#addition
+sum = (numOne) + (numTwo)
+print ("sum: " + str (sum) )
+
+#subtraction
+difference = (numOne) - (numTwo)
+print ("difference: " + str(difference))
+
+#multiplication
+product = numOne*numTwo
+print("product: " + str(product))
+
+#division
+Quotient = numOne/numTwo
+print ("Quotient: " + str(Quotient))
+
+#floor division
+Floor = numOne//numTwo
+print("Floor: " + str(Floor))
+
+#square
+square = numOne**numTwo
+print ("Square: " + str(square))
