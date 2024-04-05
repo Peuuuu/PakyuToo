b5eae213e6a264c34a205db6d65f2ad5b7b3a07e
Pei <153276259+Peuuuu@users.noreply.github.com>
2024-04-05 21:33:14 +0800

    MathOperation.py

---
+++ CC3/Midterm Exam/MathOperation.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+counter = input("Enter to be counted: ")
+concat = f"{str1} {str2}"
+print(f"""
+Sum: {num1 + num2}
+Difference: {num1 - num2}
+Product: {num1 * num2}
+Quotient: {num1 / num2}
+Floor Division: {num1 // num2}
+Modulo: {num1 % num2}
+Concatenated String: {concat}
+Concatenated Upper: {concat.upper()}
+Concatenated Lower: {concat.lower()}
+Count sa Concatenated: {concat.count(counter)}
+Count sa Concatenated: {len(concat)}""")
